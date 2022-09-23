1ad3defbe9e869bd2b8b537e6b50720cef5d524f
Gabry-76 <gabryuso88@gmail.com>
2022-09-23 17:01:47 +0200

    Fixing Bugs

--- src/Loaders/data.ts
+++ src/Loaders/data.ts
@@ -1,12 +1,19 @@
-import { readdirSync, unlink } from 'node:fs'
+import { readdirSync, unlink } from 'node:fs';
+import CDN from '../Schemes/cdn';
+import { cdnInterface } from '../Types/interfaces';
 
 export default async (): Promise<void> => {
     let hour: number = new Date().getHours()
     let minute: number = new Date().getMinutes()
 
-    if (hour == 0 && minute == 0) {
+    if (hour == 17 && minute == 0) {
         let files = await readdirSync('./Data')
         for (let file of files) {
+            let db: cdnInterface = await CDN.findOne({ ID: file })
+            if (db) {
+                db.Cached = false
+                await db.save()
+            }
             await unlink(`./Data/${file}`,  async () => {})
         }
     }
